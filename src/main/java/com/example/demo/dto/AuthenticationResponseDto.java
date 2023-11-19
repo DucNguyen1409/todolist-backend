aa6cbd83f4f3b3a9853031cbf941263dfbea808b
nguyenduc <1409.ducnguyen@gmail.com>
2023-11-19 13:46:58 +0700

    [DucNH] Change response login DTO, name project.

--- src/main/java/com/example/demo/dto/AuthenticationResponseDto.java
+++ src/main/java/com/example/demo/dto/AuthenticationResponseDto.java
@@ -1,6 +1,5 @@
 package com.example.demo.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,10 +11,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationResponseDto {
 
-    @JsonProperty("access_token")
     private String accessToken;
 
-    @JsonProperty("refresh_token")
     private String refreshToken;
 
     private String email;
